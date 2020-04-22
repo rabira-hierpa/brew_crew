5bbfc7ad8a1a5659b3023b426ee82974f71adec4
Rabra <rabira.hierpa@gmail.com>
2020-04-23 00:30:20 +0300

    Dynamic routing based on user logged in state

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -1,10 +1,14 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:brew_crew/screens/authentication/auth.dart';
+import 'package:brew_crew/screens/home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
     // Return either home or auth widget
-    return Authenticate();
+    return user == null ? Authenticate() : Home();
   }
 }
